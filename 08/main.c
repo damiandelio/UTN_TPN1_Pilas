33ac1c96f28e77775e06a7e84b50fd0fd89377c8
Damián De Lio <damiandelio@gmail.com>
2020-03-28 20:48:00 -0300

    Correcciones punto 8 y 12

--- 08/main.c
+++ 08/main.c
@@ -7,27 +7,34 @@
 int main()
 {
     Pila mazo, jugador1, jugador2;
+    int cantCartas = 0;
 
     inicpila(&mazo);
     inicpila(&jugador1);
     inicpila(&jugador2);
 
+    printf("Ingrese la cantidad de cartas a repartir: ");
+    fflush(stdin);
+    scanf("%i", &cantCartas);
+
     // rellena el mazo con cartas
-    for(int i=1; i <= 60; i++) {
+    for(int i=1; i <= cantCartas; i++) {
         apilar(&mazo, i);
     }
 
     // las reparte entre ambos jugadores
     while(!pilavacia(&mazo)) {
         apilar(&jugador1, desapilar(&mazo));
-        apilar(&jugador2, desapilar(&mazo));
+        if (!pilavacia(&mazo)) {
+            apilar(&jugador2, desapilar(&mazo));
+        }
     }
 
     // muestra las cartas de los jugadores
-    printf("jugafor1:");
+    printf("\nJUGADOR1:");
     mostrar(&jugador1);
 
-    printf("jugafor2:");
+    printf("\nJUGADOR2:");
     mostrar(&jugador2);
 
     system("pause");
--- 12/main.c
+++ 12/main.c
@@ -22,40 +22,40 @@ void ingresardatos(Pila *pila) {
     return;
 }
 
+void mover(Pila *destino, Pila *origen){
+    while(!pilavacia(origen)) {
+        apilar(destino, desapilar(origen));
+    }
+}
+
 // busca un valor dentro de una pila. Retorna 1 si lo encuentra y 0 si no lo encuentra
-int buscarValor( int val, Pila *pila, Pila *aux ) {
+int buscarValor( int val, Pila *pila ) {
+    Pila aux;
     int valTope = 0;
+
+    inicpila(&aux);
 
     while(!pilavacia(pila)) {
         valTope = desapilar(pila);
-        apilar(aux, valTope);
-
+        apilar(&aux, valTope);
         if(val == valTope) {
-            while(!pilavacia(aux)) {
-                apilar(pila, desapilar(aux));
-            }
-
+            mover(pila, &aux);
             return 1;
         }
     }
-
-    while(!pilavacia(aux)) {
-        apilar(pila, desapilar(aux));
-    }
-
+    mover(pila, &aux);
     return 0;
 }
 
 int main()
 {
-    Pila dada, modelo, aux1, aux2;
+    Pila dada, modelo, aux1;
     int valorEncontrado = 0;
     char s = 'n';
 
     inicpila(&dada);
     inicpila(&modelo);
     inicpila(&aux1);
-    inicpila(&aux2);
 
     printf("Desea ingresar datos en la pila MODELO? (s/n)");
     fflush(stdin);
@@ -70,8 +70,7 @@ int main()
     ingresardatos(&dada);
 
     while(!pilavacia(&dada)) {
-        valorEncontrado = buscarValor(tope(&dada), &modelo, &aux2);
-
+        valorEncontrado = buscarValor(tope(&dada), &modelo);
         if(valorEncontrado) {
             desapilar(&dada);
         } else {
@@ -79,9 +78,7 @@ int main()
         }
     }
 
-    while(!pilavacia(&aux1)) {
-        apilar(&dada, desapilar(&aux1));
-    }
+    mover(&dada,&aux1);
 
     printf("Pila MODELO");
     mostrar(&modelo);
