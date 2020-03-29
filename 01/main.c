9e764750bdc35929283342ebfdcd82eff072625b
Damián De Lio <damiandelio@gmail.com>
2020-03-29 00:33:17 -0300

    correcciones punto 1

--- 01/main.c
+++ 01/main.c
@@ -1,48 +1,37 @@
+/// 1. Cargar desde el teclado una pila DADA con 5 elementos.
+///    Pasar los tres primeros elementos a la pila AUX1 y los dos restantes a la pila AUX2, ambas pilas inicializadas en vacío.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "pila.h"
 
-
-void cargarpila(Pila *pila) {
-    char s = 's';
-
-    while (s == 's') {
-        leer(pila);
-        printf("Desea cargar otro dato en la pila? (s/n): ");
-        fflush(stdin);
-        scanf("%c",&s);
-    }
-    return;
-}
-
 int main()
 {
-    Pila dada;
-    Pila aux1;
-    Pila aux2;
+    Pila dada, aux1, aux2;
 
     inicpila(&dada);
     inicpila(&aux1);
     inicpila(&aux2);
 
-    // cargarpila(&dada);
-    leer(&dada);
-    leer(&dada);
-    leer(&dada);
-    leer(&dada);
-    leer(&dada);
+    // 5 ciclos de carga
+    printf("Ingrese 5 elementos a la pila DADA\n");
+    for (int i=0; i < 5; i++) {
+        leer(&dada);
+    }
 
-    mostrar(&dada);
+    for (int i =0; i < 3; i++) {
+        apilar(&aux1, desapilar(&dada));
+    }
 
-    apilar(&aux1, desapilar(&dada));
-    apilar(&aux1, desapilar(&dada));
-    apilar(&aux1, desapilar(&dada));
+    for (int i =0; i < 2; i++) {
+        apilar(&aux2, desapilar(&dada));
+    }
 
-    apilar(&aux2, desapilar(&dada));
-    apilar(&aux2, desapilar(&dada));
-
+    printf("\nPila AUX1");
     mostrar(&aux1);
+    printf("\n\nPila AUX2");
     mostrar(&aux2);
 
+    system("pause");
     return 0;
 }
